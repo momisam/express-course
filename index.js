a758adf3b13e99f143e8e752b7daca38f86c8f30
Moses Femi Smauel <mosesfemi10@gmail.com>
2025-01-14 12:17:09 +0100

    app listen

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 //create express module
 
 const express = require('express');
+const app = express();
+
+
+
+
+
+app.listen(3000, () => {
+    console.log(`server is runnig on localhost:3000`);
+});

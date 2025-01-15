2e0a2f7b10f2c0426bb536bc887e73edc0ac7f40
Moses Femi Smauel <mosesfemi10@gmail.com>
2025-01-15 15:10:44 +0100

    createing shop route

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 //create express module
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
 const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(adminRoutes);
-
+app.use(shopRoutes);
 
 
 app.listen(3000, () => {
--- routes/shop.js
+++ routes/shop.js
@@ -5,8 +5,7 @@ const router = express.Router();
 
 router.use('/', (req, res) => {
 res.send('<h1>Hello from Express</h1>');
-})
-
+});
 
 
 module.exports = router;

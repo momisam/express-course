d236fd8b767a4c1d0481785739553485cd70d526
Moses Femi Smauel <mosesfemi10@gmail.com>
2025-01-15 14:47:35 +0100

    express router

---
+++ routes/admin.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.use('/add-product', (re, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit"></form>');
+});
+
+router.use('/product', (req,res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+
+
+
+
+module.export = router;
